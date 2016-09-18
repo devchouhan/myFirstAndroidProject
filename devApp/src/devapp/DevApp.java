5cdf2e164552018586f55cbdff98673d7377a150
devchouhan <devappplay@gmail.com>
2016-09-18 23:01:55 +0530

    Update DevApp.java

--- devApp/src/devapp/DevApp.java
+++ devApp/src/devapp/DevApp.java
@@ -17,6 +17,10 @@ public class DevApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int a;
+        int b;
+        
+        
     }
     
 }
